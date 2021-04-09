17c5e4eb435096c48f2c8177f854414095d23ebf
981222 <985523426@qq.com>
2021-04-09 12:35:14 +0800

    ******

--- src/main/java/com/huang/mmall/filter/UserFilter.java
+++ src/main/java/com/huang/mmall/filter/UserFilter.java
@@ -8,7 +8,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * 继承 Filter 接口重写 doFilter() 方法实现过滤器
+ * 过滤器相关配置写在FilterConfig类中
+ */
 public class UserFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
